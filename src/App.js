1f0f73022a1dc2b3ad5b4e451a504104db7619ee
Gurjit Singh <gurjit9320@gmail.com>
2023-02-18 12:44:35 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
+import UserProfile from "./pages/UserProfile";
+import Explore from "./pages/Explore";
+import CreateBlog from "./pages/CreateBlog";
+import LoginAndSignup from "./pages/LoginAndSignup";
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <div className="">
+            <Home/>
+            <Router>
+                <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/admin" element={<Admin />} />
+                <Route exact path="/explore" element={<Explore />} />
+                <Route exact path="/profile" element={<UserProfile />} />
+                <Route exact path="/create" element={<CreateBlog />} />
+                <Route exact path="/login" element={<LoginAndSignup />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
-export default App;
+export default App
---
+++ src/Components/Blogcard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Blogcard = () => {
+  return (
+    <div className='underline'>
+      Blogcard
+    </div>
+  )
+}
+
+export default Blogcard
