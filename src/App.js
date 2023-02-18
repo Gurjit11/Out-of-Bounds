08cf2fc8f440f49ce85ffb3aa64515d0bc1a74fb
Ashish <113699160+Ashish-Shuk1a@users.noreply.github.com>
2023-02-18 17:28:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
-import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
-import home from "./pages/home";
-import Admin from "./pages/Admin";
-import UserProfile from "./pages/UserProfile";
-import Explore from "./pages/Explore";
-import CreateBlog from "./pages/CreateBlog";
-import LoginAndSignup from "./pages/LoginAndSignup";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
+import { useState } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
 
 function App() {
-    return(
-        <Router>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/admin">Admin</Link>
-                <Link to="/explore">Explore</Link>
-                <Link to="/profile">Profile</Link>
-                <Link to="/create">Create</Link>
-                <Link to="/login">Login</Link>
-                
-            </nav>
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
 
-            <Router>
-                <Route path="/" element={<home />} />
-                <Route path="/admin" element={<Admin />} />
-                <Route path="/explore" element={<Explore />} />
-                <Route path="/profile" element={<UserProfile />} />
-                <Route path="/create" element={<CreateBlog />} />
-                <Route path="/login" element={<LoginAndSignup />} />
-            </Router>
-        </Router>
-    );
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = "/login";
+    });
+  };
+
+  return (
+    <Router>
+      <nav>
+        <Link to="/"> Home </Link>
+
+        {!isAuth ? (
+          <Link to="/login"> Login </Link>
+        ) : (
+          <>
+            <Link to="/CreatePost"> Create Post </Link>
+            <button onClick={signUserOut}> Log Out</button>
+          </>
+        )}
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home isAuth={isAuth} />} />
+        <Route path="/CreatePost" element={<CreatePost isAuth={isAuth} />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
