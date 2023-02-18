8ebd1362df0560ae81ae3163f60c5701fdff15f0
Ashish <113699160+Ashish-Shuk1a@users.noreply.github.com>
2023-02-18 14:55:28 +0530

    Update UserProfile.js

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { storage } from '../firebase-config';
+import { ref, uploadBytes, listAll,getDownloadURL } from 'firebase/storage';
+import {v4} from 'uuid';
 
-const UserProfile = () => {
+function UserProfile() {
+  const [imageUpload,setImageUpload]=useState(null);
+  const [imageList, setImageList]=useState([]);
+  const imageListRef=ref(storage,"images/")
+
+  const uploadImage=()=>{
+    if(imageUpload=null) return;
+    const imageRef=ref(storage, `images/${imageUpload.name + v4()}`)
+    uploadBytes(imageRef,imageUpload).then(()=>{
+      alert("Image Uploaded");
+    });
+
+  };
+
+  useEffect(()=>{
+    listAll(imageListRef).then((response)=>{
+      response.items.forEach((item)=>{
+        getDownloadURL(item).then((url)=>{
+          setImageList((prev)=>[...prev,url]);
+        })
+      })
+    })
+  });
+
   return (
-    <div>UserProfile</div>
+    <><div>UserProfile</div><div className='UserProfile'>
+      <input type="file" onChange={(event)=>{
+        setImageUpload(event.target.files[0]);
+      }}/>
+      <button onClick={uploadImage}>Upload</button>
+    </div></>
   )
 }
 
